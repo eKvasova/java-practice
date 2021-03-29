45e165cdd5a546953e9e92682ae27a15684902a4
Evgenia <e.kvasova.88@gmail.com>
2021-03-29 21:14:20 +0300

    Exam.Exercise_1:"Converter From Seconds" - updated

--- src/exam/ex1/ConverterFromSeconds.java
+++ src/exam/ex1/ConverterFromSeconds.java
@@ -22,30 +22,51 @@ public class ConverterFromSeconds {
         long week = day / 7L;
         long remainingDays = day % 7L;
 
-        String minutes = " minute";
-        String hoursSecondLine = " hour";
-        String hoursThirdLine = " hour";
-        String days = " day";
-        String seconds = " second";
-        String weeks = " week";
+        String minutes = " минут";
+        String hoursSecondLine = " часов";
+        String hoursThirdLine = " часов";
+        String days = " суток";
+        String seconds = " секунд";
+        String weeks = " недель";
 
-        if (remainingMinutes > 1) {
-            minutes = minutes.concat("s");
+        if (((remainingSec < 10) || (remainingSec > 20)) &&
+                ((remainingMinutes == 1) || (remainingMinutes % 10 == 1))) {
+            minutes = " минута";
+        } else if (((remainingMinutes < 10) || (remainingMinutes > 20)) &&
+                ((remainingMinutes % 10 == 2) || (remainingMinutes % 10 == 3)
+                        || (remainingMinutes % 10 == 4))) {
+            minutes = " минуты";
         }
-        if (hour > 1) {
-            hoursSecondLine = hoursSecondLine.concat("s");
+        if (((hour < 10) || (hour > 20)) && ((hour == 1) || (hour % 10 == 1))) {
+            hoursSecondLine = " час";
+        } else if (((hour < 10) || (hour > 20)) &&
+                ((hour % 10 == 2) || (hour % 10 == 3) || (hour % 10 == 4))) {
+            hoursSecondLine = " часа";
         }
-        if (remainingHours > 1) {
-            hoursThirdLine = hoursThirdLine.concat("s");
+        if (((remainingHours < 10) || (remainingHours > 20)) &&
+                ((remainingHours == 1) || (remainingHours % 10 == 1))) {
+            hoursThirdLine = " час";
+        } else if (((remainingHours < 10) || (remainingHours > 20)) &&
+                ((remainingHours % 10 == 2) || (remainingHours % 10 == 3)
+                        || (remainingHours % 10 == 4))) {
+            hoursThirdLine = " часа";
         }
-        if (remainingDays > 1) {
-            days = days.concat("s");
+        if (remainingDays == 1) {
+            days = " сутки";
         }
-        if (remainingSec > 1) {
-            seconds = seconds.concat("s");
+        if (((remainingSec < 10) || (remainingSec > 20)) && ((remainingSec == 1)
+                || (remainingSec % 10 == 1))) {
+            seconds = " секунда";
+        } else if ( ((remainingSec < 10) || (remainingSec > 20)) && ((remainingSec % 10 == 2) || (remainingSec % 10 == 3) || (remainingSec % 10 == 4)
+            )) {
+            seconds = " секунды";
         }
-        if (week > 1) {
-            weeks = weeks.concat("s");
+        if (((week < 10) || (week > 20)) &&
+                ((week == 1) || (week % 10 == 1))) {
+            weeks = " неделя";
+        } else if (((week < 10) || (week > 20)) &&
+                ((week % 10 == 2) || (week % 10 == 3) || (week % 10 == 4))) {
+            weeks = " недели";
         }
         return sec +
                 "\n"
