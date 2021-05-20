4d12dc8317172f7c53a3e5d5d3ec4b692bfd2e17
Evgenia <e.kvasova.88@gmail.com>
2021-05-20 22:32:47 +0300

    Credit.Exercise_12:"Files With Text- updated"

--- src/credit/ex12/FileWithText.java
+++ src/credit/ex12/FileWithText.java
@@ -1,8 +1,10 @@
 package credit.ex12;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.newBufferedReader;
 
 public class FileWithText {
 
@@ -13,30 +15,37 @@ public class FileWithText {
         int punctuationMarksCount = 0;
 
 
-        BufferedReader br = new BufferedReader(new FileReader(".\\src\\credit\\ex12\\file.txt"));
+        try (BufferedReader br =
+                     newBufferedReader(Paths.get(".\\src\\credit\\ex12\\file.txt"))) {
 
-        while ((line = br.readLine()) != null) {
-            line = line.trim().replaceAll("\\s+", " ");
-            String[] words = line.split("[\\s\\W]+");
-            wordCount = wordCount + words.length;
+            while ((line = br.readLine()) != null) {
+                line = line.trim().replaceAll("\\s+", " ");
+                String[] words = line.split("[\\s\\W]+");
+                wordCount = wordCount + words.length;
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
         }
         System.out.println("Количество слов в тексте: " + wordCount);
-        br.close();
 
-        BufferedReader br2 = new BufferedReader(new FileReader(".\\src\\credit\\ex12\\file.txt"));
 
-        while ((line = br2.readLine()) != null) {
-            line = line.replaceAll("\\s+","").trim();
-            String[] array = line.split("");
+        try (BufferedReader br2 =
+                     newBufferedReader(Paths.get(".\\src\\credit\\ex12\\file.txt"))) {
 
-            for (String i : array) {
-                if (i.matches("\\W")) {
-                    punctuationMarksCount++;
+            while ((line = br2.readLine()) != null) {
+                line = line.replaceAll("\\s+", "").trim();
+                String[] array = line.split("");
+
+                for (String i : array) {
+                    if (i.matches("\\W")) {
+                        punctuationMarksCount++;
+                    }
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println();
         System.out.println("Количество знаков пунктуации: " + punctuationMarksCount);
-        br2.close();
+
     }
 }
